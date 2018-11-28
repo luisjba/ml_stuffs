7e28676c99a3f6fc522bff2278947ef0a15c2624
luisjba <me@luisjba.com>
2018-11-27 18:24:28 -0700

    Created a function to check if an object is numeric and create a improved version to transform numpy to tensor matrix for features

--- utils.py
+++ utils.py
@@ -11,6 +11,11 @@ import numpy as np
 import torch
 from sklearn.preprocessing import LabelEncoder
 
+def is_numeric(obj):
+    "Function to check if an object is numeric"
+    attrs = ['__add__', '__sub__', '__mul__', '__truediv__', '__pow__']
+    return all(hasattr(obj, attr) for attr in attrs)
+
 def file_list(directory, extension='jpg'):
     """
     Function that return a list of file paths for files filtered by extension
@@ -97,3 +102,66 @@ def convert_numpy_to_torch_tensors_features_matrix(data, label_encoders = []):
         if (data_found.shape[0] > 0) : # check that have data
             m_tensors[i_row,data_found[:,0]] = torch.FloatTensor(data_found[:,1]) # set the values to the corresponding columns
     return m_tensors
+
+def convert_numpy_to_torch_tensor_features_matrix(data, feature_label_encoder, filter_label_encoder=None, scale=None):
+    """
+        Function to convert numpy dataset with the data grouped by optional filter in the 0 index column and feature 
+        in the next column (or 0 if not filter provided in the index 0 column).
+        The 'y' or dependent value is taken from the last index column form the data
+        The resulted torch tensor is shape is (n,m) where: 
+            n = 1 if not filter data provided or the length of diferents classes in the filter_label_encoder provided or generated 
+                from the filter data
+            m = number of diferent features or classes in the feature_label_encoder.
+        
+        args:
+            data: 
+                Numpy array or dataframe containing the data to convert to torch tensrors. Te column 0 is the group column main identifier, and represent the differentes
+                train examples. The column 1 represent the dimensions or features. Finally the column 2 is the dependent value or the "y" variable for each feature belonging to each main identifier
+            feature_label_encoder: 
+                The Label encoder for the features classes to contruct the tensor matrix
+            filter_label_encoder:
+                Optional LabelEncoder object used to filter data and to determine the number of rows in the shape for the resulted matrix
+            scale:
+                Optional number to scale the 'y' or dependet value in the data, Usefull to scale to 0-1 for better computation in
+                Neural Network performance and accuraccy.
+        return: 
+            Torch tensor matrix with shape (n,m)
+    """
+    if type(feature_label_encoder) is not LabelEncoder:
+        raise ValueError('The parameter feature_label_encoder must be a LabelEncoder object type')
+    if len(data.shape) == 1:
+        # reshape to matrix ( size/2, size/2 ) if it is a vector
+        data = data.reshape(( int(data.shape[0]/2), int(data.shape[0]/2) ))
+    if data.shape[1] < 2:
+        raise ValueError('Yo must provide at least 2 columns  containing the categorical values and dependent value in the last column')
+    y_index = data.shape[1] - 1
+    if scale is not None:
+        data[:,y_index] = data[:,y_index] / scale
+    # Transform the feature value
+    categorical_index = 0 if y_index == 1 else 1
+    if not is_numeric(data[:,categorical_index][0]):
+        data[:,categorical_index] = feature_label_encoder.transform(data[:,categorical_index])
+    rows = 1
+    if categorical_index == 1 :
+        if filter_label_encoder is None:
+           filter_label_encoder = LabelEncoder()
+           filter_label_encoder.fit(data[:,0])
+        elif type(filter_label_encoder) is not LabelEncoder:
+            raise ValueError('The parameter filter_label_encoder must be a LabelEncoder object type')
+        if not is_numeric(data[:,0][0]):
+            data[:,0] = filter_label_encoder.transform(data[:,0])
+        rows = len(filter_label_encoder.classes_)
+    n_features = len(feature_label_encoder.classes_)
+    X = torch.zeros(rows, n_features, dtype=torch.float)
+    selected_columns = [categorical_index, y_index]
+    for i_row in range(rows):
+        data_found = data[:,selected_columns] if categorical_index == 1 else data[:,selected_columns][data[:,0] == i_row]
+        if (data_found.shape[0] > 0) :
+            X[i_row,data_found[0]] = torch.FloatTensor(data_found[:,1])
+    return X
+        
+    
+
+
+
+
